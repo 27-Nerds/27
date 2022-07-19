0ead3664efebb92ad65d44c69039bb1feb7b2136
petro <petro.polishchuk@27n.gg>
2022-07-20 01:17:45 +0300

    🥳

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,59 @@ for ( let i=0; i < draggableElems.length; i++ ) {
 // Zoom for scalable parallaxes
 
 window.addEventListener('resize', function(event){
-	console.log(event.target.innerWidth);
+	// let draggableElems = document.querySelectorAll('.zoom-elements');
+	// let draggies = []
+	// for ( let i=0; i < draggableElems.length; i++ ) {
+	// 	let draggableElem = draggableElems[i];
+	// 	draggableElem.style.transform = 'scale(' + event.target.innerWidth / 1920 + ')';
+	// }
+
+	if (window.innerWidth < 1200 && window.innerWidth > 520) {
+		lax.removeElements(".page-title");
+		lax.addElements(".page-title", {
+			scrollY: {
+				translateY: [[0, 'screenHeight'], [20, 'screenHeight*1.5']],
+				scale: [[1, 'screenHeight'], [1, 5]]
+			}
+		});
+
+		lax.removeElements(".esports-word-4");
+		lax.addElements(".esports-word-4", {
+			scrollY: {
+				translateY: [['screenHeight*2', 'screenHeight*2.75'], [0, 'screenHeight*0.9']],
+				translateX: [['screenHeight*2', 'screenHeight*2.75'], [0, '40']],
+				scale: [['screenHeight*2', 'screenHeight*2.75'], [1, 2.7]],
+			},
+		});
+	} else if (window.innerWidth >= 1200) {
+		lax.removeElements(".page-title");
+		lax.addElements(".page-title", {
+			scrollY: {
+				translateY: [[0, 'screenHeight'], [20, 'screenHeight*1.5']],
+				scale: [[1, 'screenHeight'], [1, 9]]
+			}
+		});
+
+		lax.removeElements(".esports-word-4");
+		lax.addElements(".esports-word-4", {
+			scrollY: {
+				translateY: [['screenHeight*2', 'screenHeight*2.75'], [0, 'screenHeight*0.9']],
+				translateX: [['screenHeight*2', 'screenHeight*2.75'], [0, '40']],
+				scale: [['screenHeight*2', 'screenHeight*2.75'], [1, 2.7]],
+			},
+		});
+	} else if (window.innerWidth <= 520) {
+		lax.removeElements(".page-title");
+		lax.addElements(".page-title", {
+			scrollY: {
+				translateY: [[0, 'screenHeight'], [20, 'screenHeight*1.5']],
+				scale: [[1, 'screenHeight'], [1, 2.3]]
+			}
+		});
+
+		lax.removeElements(".esports-word-4");
+	}
+
 });
 
 // Zoom for scalable parallaxes END
@@ -21,7 +73,6 @@ window.addEventListener('resize', function(event){
 // Above the clouds animations
 
 window.scrollTo(0, 0)
-
 window.onload = function () {
 
 	lax.init()
@@ -31,21 +82,70 @@ window.onload = function () {
 		function () {
 			return document.documentElement.scrollTop;
 		},
-		{ frameStep: 1, inertiaEnabled: false }
+		{
+			frameStep: 1,
+			inertiaEnabled: true
+
+		}
 	);
+
+	if (window.innerWidth < 1200 && window.innerWidth > 520) {
+		lax.addElements(".page-title", {
+			scrollY: {
+				translateY: [[0, 'screenHeight'], [20, 'screenHeight*1.5']],
+				scale: [[1, 'screenHeight'], [1, 5]]
+			}
+		});
+		lax.addElements(".esports-word-4", {
+			scrollY: {
+				translateY: [['screenHeight*2', 'screenHeight*2.75'], [0, 'screenHeight*0.9']],
+				translateX: [['screenHeight*2', 'screenHeight*2.75'], [0, '40']],
+				scale: [['screenHeight*2', 'screenHeight*2.75'], [1, 2.7]],
+			},
+		});
+	} else if (window.innerWidth >= 1200) {
+		lax.addElements(".page-title", {
+			scrollY: {
+				translateY: [[0, 'screenHeight'], [20, 'screenHeight*1.5']],
+				scale: [[1, 'screenHeight'], [1, 9]]
+			}
+		});
+		lax.addElements(".esports-word-4", {
+			scrollY: {
+				translateY: [['screenHeight*2', 'screenHeight*2.75'], [0, 'screenHeight*0.9']],
+				translateX: [['screenHeight*2', 'screenHeight*2.75'], [0, '40']],
+				scale: [['screenHeight*2', 'screenHeight*2.75'], [1, 2.7]],
+			},
+		});
+	} else if (window.innerWidth <= 520) {
+		lax.addElements(".page-title", {
+			scrollY: {
+				translateY: [[0, 'screenHeight'], [20, 'screenHeight*1.5']],
+				scale: [[1, 'screenHeight'], [1, 2.3]]
+			}
+		});
+	}
+
+	// if ( window.innerWidth > 1199 ) {
+	// 	lax.addElements(".page-title", {
+	// 		scrollY: {
+	// 			translateY: [[0, 'screenHeight'], [20, 'screenHeight*1.5']],
+	// 			scale: [[1, 'screenHeight'], [1, 9]],
+	// 		}
+	// 	});
+	// } else {
+	// 	lax.addElements(".page-title", {
+	// 		scrollY: {
+	// 			translateY: [[0, 'screenHeight'], [20, 'screenHeight*1.5']],
+	// 			scale: [[1, 'screenHeight'], [1, 5]],
+	// 		}
+	// 	});
+	// }
 
 	lax.addElements(".scroll-down", {
 		scrollY: {
 			opacity: [[0, "screenHeight/10"],[1, 0]]
 		},
-	});
-
-
-	lax.addElements(".page-title", {
-		scrollY: {
-			translateY: [[0, 'screenHeight'], [20, 'screenHeight*1.5']],
-			scale: [[1, 'screenHeight'], [1, 9]],
-		}
 	});
 
 
@@ -56,12 +156,12 @@ window.onload = function () {
 	});
 	lax.addElements(".cloud-speed-2", {
 		scrollY: {
-			translateY: [[0, 'screenHeight'], [5, '-screenHeight/3']],
+			translateY: [[0, 'screenHeight'], [5, '-screenHeight/4']],
 		},
 	});
 	lax.addElements(".cloud-speed-3", {
 		scrollY: {
-			translateY: [[0, 'screenHeight'], [5, '-screenHeight/5']],
+			translateY: [[0, 'screenHeight'], [5, '-screenHeight/6']],
 		},
 	});
 
@@ -145,11 +245,35 @@ window.onload = function () {
 			scale: [['screenHeight*2', 'screenHeight*2.75'], [1, 1.8]],
 		},
 	});
-	lax.addElements(".esports-word-4", {
+	// lax.addElements(".esports-word-4", {
+	// 	scrollY: {
+	// 		translateY: [['screenHeight*2', 'screenHeight*2.75'], [0, 'screenHeight*0.9']],
+	// 		translateX: [['screenHeight*2', 'screenHeight*2.75'], [0, '40']],
+	// 		scale: [['screenHeight*2', 'screenHeight*2.75'], [1, 2.7]],
+	// 	},
+	// });
+
+
+	lax.addElements(".services-list-item-border", {
 		scrollY: {
-			translateY: [['screenHeight*2', 'screenHeight*2.75'], [0, 'screenHeight*0.9']],
-			translateX: [['screenHeight*2', 'screenHeight*2.75'], [0, '40']],
-			scale: [['screenHeight*2', 'screenHeight*2.75'], [1, 2.7]],
+			translateX: [['elInY', 'elOutY'], [0, '100']],
+		},
+	});
+
+	// lax.addElements("h2", {
+	// 	scrollY: {
+	// 		translateX: [['elInY', 'elOutY'], [0, -10], {inertia: 5}],
+	// 	},
+	// });
+
+	lax.addElements(".board-list-item-name", {
+		scrollY: {
+			translateY: [['elInY', 'elOutY'], [0, -120]],
+		},
+	});
+	lax.addElements(".board-list-item-position", {
+		scrollY: {
+			translateY: [['elInY', 'elOutY'], [0, -120]],
 		},
 	});
 
@@ -158,19 +282,62 @@ window.onload = function () {
 // Above the clouds animations END
 
 
+// Board carousel
 
-// $(document).ready(function () {
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide('.splide', {
+		// type   : 'loop',
+		perPage: 3,
+		perMove: 1,
+		arrows: true,
+		pagination: false,
+		gap: '48px',
+		fixedWidth: '428px',
+		breakpoints: {
+			1619: {
+				fixedWidth: '324px'
 
-	// Links
+			},
+			1024: {
+				fixedWidth: '324px',
+				perPage: 2
+			},
+			520: {
+				fixedWidth: '251px',
+				perPage: 1
+			},
+		}
+	}).mount();
+});
 
-	// $('a').on("click", function (e) {
-	// 	if ($(this).attr('href') == '') {
-	// 		e.preventDefault();
-	// 	} else {
-	// 		return true;
-	// 	}
-	// });
+// Board carousel END
 
-	// Links END
 
-// });
+// Header scroll
+
+window.onscroll = function() {
+	stickyHeader();
+};
+
+let header = document.querySelector(".header");
+let stickyOffsetTop = window.innerHeight * 2;
+
+function stickyHeader() {
+	if (window.pageYOffset > stickyOffsetTop) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
+}
+
+// Header scroll END
+
+
+// Mobile Nav
+
+let mainNav = document.querySelector('.header');
+document.querySelector('.toggle-mobile-nav').addEventListener('click', function(){
+	mainNav.classList.toggle('show-mobile-nav');
+});
+
+// Mobile Nav END
